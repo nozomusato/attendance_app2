bed6bec975a148c7b79da53efaf74a91df7a6ce0
EC2 Default User <ec2-user@ip-172-31-38-175.us-east-2.compute.internal>
2019-04-27 05:18:37 +0000

    Finish Attendances edit page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,24 +7,18 @@ class UsersController < ApplicationController
     @users = User.paginate(page: params[:page])
   end
 
-  def show
-  @user = User.find(params[:id])
-  if params[:first_day].nil?
-    @first_day = Date.today.beginning_of_month
-  else
-    @first_day = Date.parse(params[:first_day])
-  end
-  @last_day = @first_day.end_of_month
-  @last_day = @first_day.end_of_month
-  (@first_day..@last_day).each do |day|
-    unless @user.attendances.any? {|attendance| attendance.worked_on == day}
-      record = @user.attendances.build(worked_on: day)
-      record.save
-      
+ def show
+    @user = User.find(params[:id])
+    @first_day = first_day(params[:first_day])
+    @last_day = @first_day.end_of_month
+    (@first_day..@last_day).each do |day|
+      unless @user.attendances.any? {|attendance| attendance.worked_on == day}
+        record = @user.attendances.build(worked_on: day)
+        record.save
+      end
     end
-  end
-  @dates = @user.attendances.where('worked_on >= ? and worked_on <= ?', @first_day, @last_day).order('worked_on')
-  @worked_sum = @dates.where.not(started_at: nil).count
+    @dates = user_attendances_month_date
+    @worked_sum = @dates.where.not(started_at: nil).count
   end
 
   def new
@@ -43,6 +37,10 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
+    @first_day = first_day(params[:date])
+    @last_day = @first_day.end_of_month
+    @dates = user_attendances_month_date
   end
 
   def update
