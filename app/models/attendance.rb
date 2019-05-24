e898cee8da007c0deecd11f233e6bd62d863771a
EC2 Default User <ec2-user@ip-172-31-38-175.us-east-2.compute.internal>
2019-05-24 19:21:37 +0000

    バリデーション機能追加

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -1,12 +1,19 @@
 class Attendance < ApplicationRecord
   belongs_to :user
   validates :worked_on, presence: true
-  validate :started_at_none?
   
-  def started_at_none?
-      if day.started_at.nil? 
-       errors.add(:finished_at, ":出勤時間がありません。")
-      end
+  
+  validate :finished_at_edit_valid
+   def finished_at_edit_valid
+    if started_at.nil? && finished_at.present?
+     errors.add(:finished_at, "出社時間が入力されていません。")
+    end
+   end
+   
+  validate :finished_at_cannot_be_faster_than_started_at
+  def finished_at_cannot_be_faster_than_started_at
+    if  started_at.present? && started_at > finished_at
+      errors.add(:finished_at, "退勤時間が出勤時間より早くなっています。")
+    end
   end
-  
 end
